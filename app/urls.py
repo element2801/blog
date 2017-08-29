9b306f8a1a625d10a5f1dbc07ff727d8c4d47ccb
Nikolay <element280105@gmail.com>
2017-08-29 15:02:08 +0300

    create empty blog app

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('blog.urls')),
     url(r'^logout', auth_views.logout, {'template_name': 'app/login.html', 'next_page': 'login'}, name='logout'),
     url(r'^accounts/login', auth_views.login, {'template_name': 'app/login.html'}, name='login')
 ]
