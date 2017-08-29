f92cdc0e3349bee6de425d2fd6ce1b3b8d413d47
Nikolay <element280105@gmail.com>
2017-08-29 14:56:05 +0300

    create django project

---
+++ app/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^logout', auth_views.logout, {'template_name': 'app/login.html', 'next_page': 'login'}, name='logout'),
+    url(r'^accounts/login', auth_views.login, {'template_name': 'app/login.html'}, name='login')
+]
